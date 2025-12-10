e84fbcd742c87b228220a2fe36de9682805faa1d
Ihor Kuzmenko <nameless1306@gmail.com>
2025-12-10 15:39:04 +0200

    Create project

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  root: "src",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
+    },
+  },
+});
