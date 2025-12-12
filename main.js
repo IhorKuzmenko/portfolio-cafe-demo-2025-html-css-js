538a3af141d8af6db9a648ca41c0e3cdd84210ba
Ihor Kuzmenko <nameless1306@gmail.com>
2025-12-12 17:13:47 +0200

    Change reference

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 axios
-  .get("/data/menu.json")
+  .get("/portfolio-cafe-demo-2025-html-css-js/data/menu.json")
   .then((response) => console.log(response.data))
   .catch((error) => console.log(error));
